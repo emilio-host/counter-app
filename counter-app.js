708f361c15ca05a02e1615ef52e93dbfb62c9aa7
emilio-host <ecs5756@psu.edu>
2024-10-03 21:41:40 -0400

    Add files via upload

---
+++ counter-app.js
@@ -0,0 +1,81 @@
+import { LitElement, html, css } from "lit";
+import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+
+export class counterApp extends DDDSuper(LitElement) {
+
+  static get tag() {
+    return "counter-app";
+  }
+
+  constructor() {
+    super();
+    this.title = "";
+    this.counter = 16;
+    this.min = 10;
+    this.max = 25;
+  }
+
+  static get properties() {
+    return {
+      title: { type: String },
+      counter: { type: Number },
+      min: { type: Number },
+      max: { type: Number }
+    };
+  }
+
+  static get styles() {
+    return [super.styles,
+    css`
+      :host {
+        display: block;
+        color: var(--ddd-theme-primary);
+        background-color: var(--ddd-theme-accent);
+        font-family: var(--ddd-font-navigation);
+        font-size: var(--counter-app-font-size, var(--ddd-font-size-s));
+      }
+      .wrapper {
+        margin: var(--ddd-spacing-2);
+        padding: var(--ddd-spacing-4);
+      }
+      div {
+        padding: 0;
+        margin: 0;
+      }
+    `];
+  }
+
+  decrement() {
+    if (this.counter > this.min) {
+      this.counter -= 1;
+    } 
+  }
+
+  increment() {
+    if (this.counter < this.max) {
+      this.counter += 1;
+    }
+  }
+
+  render() {
+    return html`
+<div class="wrapper">
+  <div>${this.title}</div>
+
+  <div class="counter-value">${this.counter}</div>
+    <button @click="${this.decrement}">-</button>
+    <button @click="${this.increment}">+</button>
+  <slot></slot>
+</div>`;
+  }
+
+  /**
+   * haxProperties integration via file reference
+   */
+  static get haxProperties() {
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
+      .href;
+  }
+}
+
+globalThis.customElements.define(counterApp.tag, counterApp);
